e1804b4a65920411f9a02402dd445ba6f458b30e
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-06-13 00:09:28 +0200

    Adding the rabbitmq module to send\receive messages

---
+++ rabbitholer/rabbit_dumper.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import pika
+
+
+
+
+class RabbitDumper:
+
+    def __init__(self, exchange, queue, routing_key, server):
+
+        self.exchange = exchange
+        self.queue = queue
+        self.routing_key = routing_key
+        self.server = server
+        
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=server))
+        self.channel = self.connection.channel()
+
+        self.channel.exchange_declare(exchange=self.exchange,
+                                 exchange_type='fanout',
+                                 passive=False,
+                                 durable=False,
+                                 auto_delete=False)
+
+        self.channel.queue_declare(queue=self.queue, auto_delete=False)
+
+        self.channel.queue_bind(exchange=self.exchange,
+                                queue=self.queue,
+                                routing_key=self.routing_key)
+
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.destroy()
+
+    def new_msg(self, ch, method, properties, body):
+        self.callback(body.decode("utf-8"))
+
+
+
+    def send(self, msg):
+
+        props = pika.spec.BasicProperties(expiration='30000')
+        
+        self.channel.basic_publish(
+            exchange=self.exchange,
+            routing_key=self.routing_key,
+            body=msg,
+            properties=props)
+        
+
+
+    def receive(self, callback):
+        self.callback = callback
+        self.channel.basic_consume(
+            queue=self.queue, on_message_callback=self.new_msg, auto_ack=True)
+        self.channel.start_consuming()
+        
+
+        
+        pass
+
+
+    def destroy(self):
+        self.channel.close()
+        self.connection.close()
+        
+        
+        
